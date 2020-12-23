5d9d3f410179e5df4f75b27462c3e21e86b1ce9a
Rene Zbinden <rene.zbinden@postfinance.ch>
2020-12-23 13:58:35 +0100

    chore: moved from private repo to public github repo

---
+++ flash.go
@@ -0,0 +1,204 @@
+// Package flash configures an opinionated zap logger.
+package flash
+
+import (
+	"fmt"
+	"sync"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+// Logger is the flash logger which embeds a zap.SugaredLogger.
+type Logger struct {
+	*zap.SugaredLogger
+	atom              zap.AtomicLevel
+	m                 sync.Mutex
+	currentLevel      zapcore.Level
+	disableStackTrace bool
+}
+
+// Option configures zap.Config.
+type Option func(c *config)
+
+// WithColor enables color output.
+func WithColor() Option {
+	return func(c *config) {
+		c.enableColor = true
+	}
+}
+
+// WithoutCaller stops annotating logs with the calling function's file
+// name and line number.
+func WithoutCaller() Option {
+	return func(c *config) {
+		c.disableCaller = true
+	}
+}
+
+// WithSink changes the default zap stderr sink.
+func WithSink(name string) Option {
+	return func(c *config) {
+		c.sink = name
+	}
+}
+
+// WithDebug enables or disables debug log level.
+func WithDebug(debug bool) Option {
+	return func(c *config) {
+		c.isDebug = debug
+	}
+}
+
+// WithStacktrace completely enables automatic stacktrace capturing. Stacktraces
+// are captured ErrorLevel and above in production.
+func WithStacktrace() Option {
+	return func(c *config) {
+		c.disableStacktrace = false
+	}
+}
+
+// WithPrometheus registers a prometheus log message counter.
+//
+// The created metrics are of the form:
+//     <appName>_log_messages_total{level="info"} 4
+func WithPrometheus(appName string, registry prometheus.Registerer) Option {
+	return func(c *config) {
+		counter := prometheus.NewCounterVec(
+			prometheus.CounterOpts{
+				Name: fmt.Sprintf("%s_log_messages_total", appName),
+				Help: "How many log messages created, partitioned by log level.",
+			},
+			[]string{"level"},
+		)
+		registry.MustRegister(counter)
+
+		c.hook = func(e zapcore.Entry) error {
+			counter.WithLabelValues(e.Level.String()).Inc()
+			return nil
+		}
+	}
+}
+
+// New creates a new Logger.
+func New(opts ...Option) *Logger {
+	l := zap.New(nil) // noop logger
+	atom := zap.NewAtomicLevelAt(zap.InfoLevel)
+
+	cfg := config{
+		disableStacktrace: true,
+	}
+
+	for _, opt := range opts {
+		opt(&cfg)
+	}
+
+	zapConfig := zap.NewProductionConfig()
+	zapConfig.DisableStacktrace = cfg.disableStacktrace
+	zapConfig.Sampling = nil
+	zapConfig.Encoding = "console"
+	zapConfig.DisableCaller = cfg.disableCaller
+	zapConfig.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	zapConfig.EncoderConfig.EncodeDuration = zapcore.StringDurationEncoder
+	zapConfig.EncoderConfig.EncodeLevel = zapcore.CapitalLevelEncoder
+	zapConfig.Level = atom
+
+	if cfg.enableColor {
+		zapConfig.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+	}
+
+	if cfg.sink != "" {
+		zapConfig.OutputPaths = []string{cfg.sink}
+	}
+
+	var err error
+
+	l, err = zapConfig.Build()
+	if err != nil {
+		panic(fmt.Sprintf("could not create zap logger: %s", err))
+	}
+
+	stackTraceLevel := zap.FatalLevel
+
+	if cfg.isDebug {
+		atom.SetLevel(zap.DebugLevel)
+		stackTraceLevel = zap.ErrorLevel
+	}
+
+	// fix level for stack traces
+	if !cfg.disableStacktrace {
+		l = l.WithOptions(zap.AddStacktrace(stackTraceLevel))
+	}
+
+	if cfg.hook != nil {
+		l = l.WithOptions(zap.Hooks(cfg.hook))
+	}
+
+	defer func() {
+		_ = l.Sync()
+	}()
+
+	return &Logger{
+		SugaredLogger:     l.Sugar(),
+		atom:              atom,
+		disableStackTrace: cfg.disableStacktrace,
+	}
+}
+
+// SetDebug enables or disables debug level.
+func (l *Logger) SetDebug(d bool) {
+	level := zap.DebugLevel
+	stackTraceLevel := zap.ErrorLevel
+
+	if !d {
+		l.m.Lock()
+		level = l.currentLevel
+		l.m.Unlock()
+
+		stackTraceLevel = zap.FatalLevel
+	}
+
+	l.atom.SetLevel(level)
+	l.stackTrace(stackTraceLevel)
+}
+
+func (l *Logger) stackTrace(lvl zapcore.Level) {
+	if l.disableStackTrace {
+		return
+	}
+
+	l.m.Lock()
+	l.SugaredLogger = l.Get().Desugar().WithOptions(zap.AddStacktrace(lvl)).Sugar()
+	l.m.Unlock()
+}
+
+// Disable disables (nearly) all output.
+func (l *Logger) Disable() {
+	l.m.Lock()
+	l.currentLevel = zapcore.FatalLevel
+	l.m.Unlock()
+	l.atom.SetLevel(zap.FatalLevel)
+}
+
+// SetLevel sets the chosen level.
+func (l *Logger) SetLevel(level zapcore.Level) {
+	l.m.Lock()
+	l.currentLevel = level
+	l.m.Unlock()
+	l.atom.SetLevel(level)
+}
+
+// Get returns the embedded zap.Logger
+func (l *Logger) Get() *zap.SugaredLogger {
+	return l.SugaredLogger
+}
+
+type config struct {
+	enableColor       bool
+	disableCaller     bool
+	disableStacktrace bool
+	isDebug           bool
+	hook              func(zapcore.Entry) error
+	sink              string
+}
---
+++ flash_test.go
@@ -0,0 +1,183 @@
+package flash_test
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"net/url"
+	"os"
+	"reflect"
+	"regexp"
+	"strings"
+	"testing"
+
+	"github.com/postfinance/flash"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/testutil"
+	"github.com/stretchr/testify/require"
+	"github.com/tj/assert"
+	"go.uber.org/zap"
+)
+
+// nolint: gochecknoglobals
+var sink *memorySink
+
+func TestMain(m *testing.M) {
+	sink = &memorySink{new(bytes.Buffer)}
+	err := zap.RegisterSink("memory", func(*url.URL) (zap.Sink, error) {
+		return sink, nil
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	os.Exit(m.Run())
+}
+
+func TestDefault(t *testing.T) {
+	defer sink.Reset()
+
+	l := flash.New(flash.WithSink("memory://"))
+	l.Debug("debug")
+
+	e := sink.parse()
+	assert.Len(t, e, 0, "debug message logged")
+	l.Info("info")
+
+	e = sink.parse()
+	assert.Equal(t, "INFO", e[0].level)
+	assert.Equal(t, "info", e[0].msg)
+	assert.NotEmpty(t, e[0].caller)
+}
+
+func TestWithColor(t *testing.T) {
+	defer sink.Reset()
+
+	l := flash.New(flash.WithSink("memory://"), flash.WithColor())
+
+	l.Info("info")
+
+	const blue = `1b[34m`
+
+	output := sink.String()
+	assert.True(t, strings.Contains(fmt.Sprintf("%q", output), blue))
+}
+
+func TestWithoutCaller(t *testing.T) {
+	defer sink.Reset()
+
+	l := flash.New(flash.WithSink("memory://"), flash.WithoutCaller())
+	l.Info("info")
+	require.NotEmpty(t, sink.String())
+	e := sink.parse()
+	assert.Empty(t, e[0].caller)
+}
+
+func TestWithStacktraceWithDebug(t *testing.T) {
+	defer sink.Reset()
+
+	l := flash.New(flash.WithSink("memory://"), flash.WithDebug(true), flash.WithStacktrace())
+	l.Info("info")
+	// only stacktraces for error in debug mode
+	assert.False(t, sink.containsStackTrace(), "stack trace detected")
+	l.Error("error")
+	assert.True(t, sink.containsStackTrace(), "stack trace detected")
+}
+
+func TestSetDebugWithStacktrace(t *testing.T) {
+	defer sink.Reset()
+
+	l := flash.New(flash.WithSink("memory://"), flash.WithStacktrace())
+	l.Debug("debug")
+	assert.Len(t, sink.String(), 0, "debug message logged")
+	l.Error("error")
+	sink.Reset()
+	assert.False(t, sink.containsStackTrace(), "stack trace detected")
+	l.SetDebug(true)
+	l.Debug("debug")
+	assert.NotEmpty(t, sink.String(), "no debug message logged")
+	l.Error("error")
+	assert.True(t, sink.containsStackTrace(), "stack trace detected")
+}
+
+func TestWithPrometheus(t *testing.T) {
+	defer sink.Reset()
+
+	r := prometheus.NewRegistry()
+
+	l := flash.New(flash.WithSink("memory://"), flash.WithPrometheus("appname", r))
+
+	l.Info("info")
+	l.Info("info")
+	l.Error("error")
+	l.Debug("debug")
+
+	const metadata = `
+		# HELP appname_log_messages_total How many log messages created, partitioned by log level.
+        # TYPE appname_log_messages_total counter
+	`
+
+	expected := `
+		appname_log_messages_total{level="error"} 1
+		appname_log_messages_total{level="info"} 2
+	`
+
+	err := testutil.GatherAndCompare(r, strings.NewReader(metadata+expected), "appname_log_messages_total")
+	require.NoError(t, err, "unexpected collecting result")
+	l.SetDebug(true)
+	l.Debug("debug")
+
+	expected = `
+		appname_log_messages_total{level="debug"} 1
+		appname_log_messages_total{level="error"} 1
+		appname_log_messages_total{level="info"} 2
+	`
+	err = testutil.GatherAndCompare(r, strings.NewReader(metadata+expected), "appname_log_messages_total")
+	require.NoError(t, err, "unexpected collecting result")
+}
+
+type memorySink struct {
+	*bytes.Buffer
+}
+
+// implement the zap.Sink interface.
+func (m *memorySink) Close() error { return nil }
+func (m *memorySink) Sync() error  { return nil }
+
+func (m *memorySink) parse() []entry {
+	s := bufio.NewScanner(m)
+	s.Split(bufio.ScanLines)
+
+	entries := []entry{}
+	callerRegexp := regexp.MustCompile(`\.go:\d+`)
+
+	for s.Scan() {
+		e := entry{}
+		fields := strings.Fields(s.Text())
+		e.level = fields[1]
+		msgIndex := 2
+
+		if callerRegexp.MatchString(fields[2]) {
+			e.caller = fields[2]
+			msgIndex++
+		}
+
+		e.msg = strings.Join(fields[msgIndex:], `\t`)
+		entries = append(entries, e)
+	}
+
+	return entries
+}
+
+func (m *memorySink) containsStackTrace() bool {
+	currentPackageName := reflect.TypeOf(memorySink{}).PkgPath()
+
+	return strings.Contains(m.String(), currentPackageName)
+}
+
+type entry struct {
+	level  string
+	caller string
+	msg    string
+}
